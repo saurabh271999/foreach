f979050d6cb9f953f1bfe975f1366213925fd8dc
saurabh bhardwaj <saurabhpandat27@gmail.com>
2025-01-26 01:05:39 +0530

    filter method complete

--- script.js
+++ script.js
@@ -1,7 +1,60 @@
-const arr = [22,24,45,45,56]
 
-arr.map((item) => {
-    console.log(arr)
-    return item*5
-})
+
+//  const values = arr.forEach((item) => {
+//     // console.log(arr)
+//     return item
+// })
+// const myNums = [22,24,90,45,56,12,,67,78]
+
+// const newNums = myNums.filter((num) => {
+//    return num > 40
+// })
+
+// const newNums = []
+
+// myNums.forEach((num) => {
+//     if(num>40){
+//         newNums.push(num)
+//     }
+// })
+// console.log(newNums)
+const users = [
+    {
+      id: 1,
+      name: "John Doe",
+      age: 28,
+      email: "john.doe@example.com",
+      isActive: true,
+      hobbies: ["reading", "hiking", "swimming"]
+    },
+    {
+      id: 2,
+      name: "Jane Smith",
+      age: 45,
+      email: "jane.smith@example.com",
+      isActive: false,
+      hobbies: ["cooking", "yoga"]
+    },
+    {
+      id: 3,
+      name: "Alice Johnson",
+      age: 24,
+      email: "alice.johnson@example.com",
+      isActive: true,
+      hobbies: ["photography", "traveling"]
+    },
+    {
+      id: 4,
+      name: "Bob Brown",
+      age: 45,
+      email: "bob.brown@example.com",
+      isActive: false,
+      hobbies: ["golf", "fishing"]
+    }
+  ];
+
+  const findData = users.filter((item) => item.age === 45)
+  console.log(findData)
+
+
 
